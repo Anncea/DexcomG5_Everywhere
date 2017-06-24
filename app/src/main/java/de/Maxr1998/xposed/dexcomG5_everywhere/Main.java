d01faffbeed2a1cd6f4b3aa0b1676a49f060af2c
Maxr1998 <max.rumpf1998@gmail.com>
2017-06-24 13:47:34 +0200

    Use a different hack and fix Dexcom's weird password requirements
    (only alphanumeric)

--- app/src/main/java/de/Maxr1998/xposed/dexcomG5_everywhere/Main.java
+++ app/src/main/java/de/Maxr1998/xposed/dexcomG5_everywhere/Main.java
@@ -1,14 +1,16 @@
 package de.Maxr1998.xposed.dexcomG5_everywhere;
 
+import android.os.Build;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static de.robv.android.xposed.XposedBridge.log;
-import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.setStaticObjectField;
 
 public class Main implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 
@@ -25,20 +27,31 @@ public class Main implements IXposedHookZygoteInit, IXposedHookLoadPackage {
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lPParam) throws Throwable {
         if (lPParam.packageName.equals(PACKAGE_NAME)) {
             try {
-                Class activity = findClass(BASE_NAME + ".activities.AppCompatabilityActivity", lPParam.classLoader);
+                setStaticObjectField(Build.class, "MANUFACTURER", "motorola");
+                setStaticObjectField(Build.class, "MODEL", "Nexus 6");
 
-                findAndHookMethod(activity, "performAppValidity", new XC_MethodReplacement() {
-                    @Override
-                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                        callMethod(param.thisObject, "goDirectlyToNextActivity");
-                        return null;
-                    }
-                });
+                Class activity = findClass(BASE_NAME + ".activities.AppCompatabilityActivity", lPParam.classLoader);
+                Class loginActivity = findClass(BASE_NAME + ".activities.LoginActivity", lPParam.classLoader);
+
 
                 findAndHookMethod(activity, "isRooted", new XC_MethodReplacement() {
                     @Override
                     protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
                         return false;
+                    }
+                });
+
+                findAndHookMethod(loginActivity, "isUsernameValid", String.class, new XC_MethodReplacement() {
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                        return true;
+                    }
+                });
+
+                findAndHookMethod(loginActivity, "isPasswordValid", String.class, new XC_MethodReplacement() {
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                        return true;
                     }
                 });
             } catch (Throwable t) {
